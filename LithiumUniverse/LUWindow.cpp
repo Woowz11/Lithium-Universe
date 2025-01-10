092b3eb8b59c07d892e42f6953e9924cb2a67735
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2025-01-10 22:43:28 +0300

    0.0.0c

--- LithiumUniverse/LUWindow.cpp
+++ LithiumUniverse/LUWindow.cpp
@@ -1,75 +1,25 @@
-#include <Windows.h>
+#include <GLFW/glfw3.h>
 #include "Console.h";
-
-const LPCWSTR GameWindowClass = L"GameWindow";
-
-LRESULT CALLBACK GameWindowLife(HWND id, UINT msg, WPARAM param, LPARAM lparam) {
-	switch (msg)
-	{
-	case WM_CLOSE:
-		DestroyWindow(id);
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(id, msg, param, lparam);
-	}
-	return 0;
-}
-
-int WINAPI GameWindow(HINSTANCE inst, HINSTANCE prev, LPSTR cmdline, int cmdshow) {
-	WNDCLASSEX win;
-	HWND id;
-	MSG msg;
-
-	win.cbSize        = sizeof(WNDCLASSEX);
-	win.style         = 0;
-	win.lpfnWndProc   = GameWindowLife;
-	win.cbClsExtra    = 0;
-	win.cbWndExtra    = 0;
-	win.hInstance     = inst;
-	win.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
-	win.hCursor       = LoadCursor(NULL, IDC_ARROW);
-	win.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	win.lpszMenuName  = NULL;
-	win.lpszClassName = GameWindowClass;
-	win.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
-
-	if (!RegisterClassEx(&win)) {
-		MessageBox(NULL, L"GOVNO POTOM SDELAY!", L"GOVNO!",
-			MB_ICONEXCLAMATION | MB_OK);
-		return 0;
-	}
-
-	id = CreateWindowEx(
-		WS_EX_CLIENTEDGE,
-		GameWindowClass,
-		L"LithiumUniverse (0.0.0b)",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
-		NULL, NULL, inst, NULL
-	);
-
-	if (id == NULL) {
-		MessageBox(NULL, L"GOVNO POTOM SDELAY! 2", L"GOVNO! 2",
-			MB_ICONEXCLAMATION | MB_OK);
-		return 0;
-	}
-
-	ShowWindow(id, cmdshow);
-	UpdateWindow(id);
-
-	Print("Window created!");
-
-	while (GetMessage(&msg, NULL, 0, 0) > 0) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	return msg.wParam;
-}
+#include "GlobalRender.h";
 
 int CreateGameWindow() {
-	return GameWindow(GetModuleHandle(NULL), NULL, GetCommandLineA(), SW_SHOW);
+	GLFWwindow* Window;
+	
+	if (!glfwInit()) {
+		return -1;
+	}
+	
+	Window = glfwCreateWindow(800, 600, "LithiumUniverse (0.0.0c)", NULL, NULL);
+	if (!Window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(Window);
+	while (!glfwWindowShouldClose(Window)) {
+		glfwSwapBuffers(Window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
 }
