303fb31a8d9e9cab3ae8019910e0d04a7b721ecd
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2025-01-12 12:08:48 +0300

    0.1.0T

--- LithiumUniverse/BaseConstants.cpp
+++ LithiumUniverse/BaseConstants.cpp
@@ -8,7 +8,7 @@
  */
 const int GameVersionMajor = 0;
 const int GameVersionMinor = 1;
-const int GameVersionPatch = 17;
+const int GameVersionPatch = 19;
 
 std::string GetGameVersionPatchWithLetters() {
     const std::string alphabet = "ABCDEFGHIJKLMNoPQRSTUVWXYZ";
--- Resources/CompileShaders.bat
+++
@@ -1,3 +0,0 @@
-F:\vulkan\Bin\glslc.exe base.vert -o vert.spv
-F:\vulkan\Bin\glslc.exe base.frag -o frag.spv
-pause
